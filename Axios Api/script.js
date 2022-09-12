875d6b94e4b9f7b1ce4fc50aa7989bdda5e9e0ee
Chandan Ojha <chandanojha776@gmail.com>
2022-09-12 10:07:06 +0600

    axios api practice completed

--- Axios Api/script.js
+++ Axios Api/script.js
@@ -70,7 +70,70 @@ console.clear();
     .catch((err) => console.log(err));*/
 
 //delete data
-axios
+/* axios
     .delete("https://jsonplaceholder.typicode.com/posts/1")
     .then((res) => console.log(res.data))
-    .catch((err) => console.log(err));
+    .catch((err) => console.log(err)); */
+
+
+/** Axios Using by async await **/
+
+const makeRequest = async (config) => {
+   return await axios(config);
+};
+
+//get data
+/*const getData = () => {
+   makeRequest("https://jsonplaceholder.typicode.com/posts")
+       .then((res) => console.log(res.data))
+       .catch((err) => console.log(err))
+};
+
+getData();*/
+
+//create data
+/*const createData = () => {
+   makeRequest({
+      url: 'https://jsonplaceholder.typicode.com/posts',
+      method: 'post',
+      data: JSON.stringify({
+         title: 'foo',
+         body: 'bar',
+         userId: 1,
+      }),
+   })
+       .then((res) => console.log(res.data))
+       .catch((err) => console.log(err))
+}
+
+createData();*/
+
+//update data
+/*const updateData = () => {
+   makeRequest({
+      url: 'https://jsonplaceholder.typicode.com/posts/1',
+      method: 'put',
+      data: JSON.stringify({
+         id: 1,
+         title: 'foo updated',
+         body: 'bar',
+         userId: 1,
+      }),
+   })
+       .then((res) => console.log(res.data))
+       .catch((err) => console.log(err))
+}
+
+updateData();*/
+
+//delete data
+const deleteData = () => {
+   makeRequest({
+      url: 'https://jsonplaceholder.typicode.com/posts/1',
+      method: 'delete'
+   })
+       .then((res) => console.log(res.data))
+       .catch((err) => console.log(err))
+}
+deleteData();
+
